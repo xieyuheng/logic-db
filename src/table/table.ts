bae8e0f566b113bd16338e9b5564ffda2e9f7bd8
Xie Yuheng <xyheme@gmail.com>
2021-08-14 18:15:57 +0800

    up

--- src/table/table.ts
+++ src/table/table.ts
@@ -21,4 +21,31 @@ export class Table<T> {
   i(data: Logical<T>): void {
     this.rows.push(data)
   }
+
+  o(data: Logical<T>): Proposition {
+    return new Proposition()
+  }
+
+  query(pattern: Logical<T>, opts?: { limit?: number }) {
+    // let var_map = new Map
+    // let data = pattern_to_data_with_var_map (pattern, var_map)
+    // let searching = new searching_t ([
+    //   new prop_row_t (new subst_t, [this.o (data)])
+    // ])
+    // let solutions = searching
+    //   .take_subst(opts?.limit)
+    //   .map ((subst) => {
+    //     let sol = {}
+    //     for (let name of var_map.keys ()) {
+    //       sol [name] = subst.reify (
+    //         var_map.get (name))
+    //     }
+    //     return sol
+    //   })
+    // let query_res = new query_res_t
+    // query_res.solutions = solutions
+    // return query_res
+  }
 }
+
+class Proposition {}
