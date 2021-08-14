47ab902c4e83e6f14c75ea59ab2d74cf2a427743
Xie Yuheng <xyheme@gmail.com>
2021-08-14 17:29:11 +0800

    up

--- src/table/table.ts
+++ src/table/table.ts
@@ -1,3 +1,11 @@
+import ty from "@xieyuheng/ty"
+import { Schema } from "@xieyuheng/ty"
+
 export class Table<T> {
   rows: Array<T> = []
 }
+
+// const user = table.create("user", )
+
+// user.fact({ id: 1, name: "xieyuheng"})
+// user.fact({ id: 2, name: "xieyuheng"})
