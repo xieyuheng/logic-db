1b7f0295e669f1927f265959425291a7f51644dd
Xie Yuheng <xyheme@gmail.com>
2021-08-17 00:12:51 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 export * from "./value"
 export * from "./table"
 
-export { default as ty } from "@xieyuheng/ty"
-export { Schema } from "@xieyuheng/ty"
+// NOTE API to build goals
 
 import * as Goals from "./goals"
 
 export const eq = Goals.Unifiable.create
 export const ne = Goals.NotUnifiable.create
+
+// NOTE re-export dependencies
+
+export { default as ty } from "@xieyuheng/ty"
+export { Schema } from "@xieyuheng/ty"
